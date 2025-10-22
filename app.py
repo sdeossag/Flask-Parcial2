d31b01449658c012739626464e0f58d0c0c39767
sdeossag <sdeossag@eafit.edu.co>
2025-10-22 07:48:11 -0500

    Correcion Factorial

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
+import math
 
 app = Flask(__name__)
 
 @app.route('/numero/<int:num>')
 def evaluar_numero(num):
+    factorial = math.factorial(num)
     etiqueta = "par" if num % 2 == 0 else "impar"
     return jsonify({
         "numero": num,
+        "factorial": factorial,
         "etiqueta": etiqueta
     })
 
